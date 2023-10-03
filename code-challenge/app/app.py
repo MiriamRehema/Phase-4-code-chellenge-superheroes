f75b4a51ab59512cc2bd7eaed65bae643b8d22c2
MiriamRehema <rehemamiriam30@gmail.com>
2023-10-03 10:19:28 +0300

    app.py

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -3,10 +3,10 @@
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db, Hero,Power
+from models import db, Hero,Power,HeroPower
 from flask import Flask, request, make_response, jsonify
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Superheroes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
@@ -27,6 +27,24 @@ def heroes():
             jsonify([heroe.to_dict() for heroe in heroes]),
             200,
         )
+
+
+
+@app.route('/heroes/<int:id>')
+def hero_by_id(id):
+    hero = Hero.query.get(id)
+    if hero:
+        hero_dict = {
+            "id": hero.id,
+            "name": hero.name,
+            "super_name": hero.super_name
+
+        }
+        return jsonify(hero_dict), 200
+    else:
+        return jsonify({"error": "Hero not found"}), 404
+    
+
     
 @app.route('/powers', methods=['GET'])
 def powers():
@@ -38,7 +56,69 @@ def powers():
             200,
         )
    
+@app.route('/powers/<int:id>')
+def powers_by_id(id):
+    power = Power.query.get(id)
+    if not power:
+        power_dict = {
+            "id": power.id,
+            "name": power.name,
+            "description": power.description
 
+        }
+        return jsonify(power_dict), 200
+    #else:
+        #return jsonify({"error": "Power not found"}), 404
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        if "description" in data:
+            power.description= data["Updated description"]
+       
+        db.session.commit()
+        restaurant_dict = {
+            "id": power.id,
+            "name":power.name,
+            "description":power.description
+        }
+        return jsonify(restaurant_dict), 200
+    else:
+        return jsonify({"error":"Validation errors"})
+    
+
+
+
+@app.route('/hero_powers', methods=['POST'])
+def create_hero_power():
+    data = request.get_json()
+
+    if not all(key in data for key in ("strength", "power_id", "hero_id")):
+        return jsonify({"errors": ["validation errors.include all keys"]}), 400
+    #strength=data["strength"]
+    hero_id = data["hero_id"]
+    power_id = data["power_id"]
+
+    hero = Hero.query.get(hero_id)
+    power = Power.query.get(power_id)
+    
+    if not hero or not power:
+        return jsonify({"errors": ["validation errors pizza and restaurant don't exist"]}), 400
+
+    hero_powers = HeroPower(
+        strength=data["strength"],
+        hero_id=data["hero_id"],
+        power_id=data["power_id"]
+    )
+    db.session.add(hero_powers)
+    db.session.commit()
+
+    power_data = {
+        "id": power.id,
+        "name": power.name,
+        "description": power.description
+    }
+
+    return jsonify(power_data), 201
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
